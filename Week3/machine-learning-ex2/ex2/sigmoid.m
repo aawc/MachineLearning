58153383b53056d1055e06ac78f294d0faf62e26
Varun Khaneja <ca.aawc@yahoo.com>
2015-10-01 18:09:23 -0700

    Implementation for sigmoid

--- Week3/machine-learning-ex2/ex2/sigmoid.m
+++ Week3/machine-learning-ex2/ex2/sigmoid.m
@@ -8,11 +8,11 @@ g = zeros(size(z));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
-
-
-
-
-
+for row=1:size(z,1),
+  for col=1:size(z,2),
+    g(row, col) = 1/(1 + exp(-z(row, col)));
+  end
+end
 % =============================================================
 
 end
